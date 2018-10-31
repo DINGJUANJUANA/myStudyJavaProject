ee86be22fbe539f40d887f1e29faddac696d3568
dingjuanjuan <dingjuanjuan@163.com>
2018-10-31 20:33:24 +0800

    test

--- ssmstudy/src/main/java/com/ssmstudy/controller/BillController.java
+++ ssmstudy/src/main/java/com/ssmstudy/controller/BillController.java
@@ -1,7 +1,18 @@
 package com.ssmstudy.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.ssmstudy.po.Bill;
+import com.ssmstudy.service.BillService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
 
 /**
  * Created by Administrator on 2018-10-31.
@@ -10,8 +21,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "bill")
 public class BillController {
 
+    @Autowired
+    private BillService billService;
     @RequestMapping(value = "list")
     public String list(){
         return "bill/list";
+    }
+
+    //,method = RequestMethod.POST
+   @ResponseBody
+    @RequestMapping(value = "getList")
+    public String getList(Bill queryParams){
+       String test= queryParams.getDescription();
+       System.out.println("查询参数"+test);
+        List<Bill> billList=billService.getList();
+        String datajson= JSON.toJSONString(billList);
+        //服务器断分页数据源
+        String  result="{\"total\":" +String.valueOf(billList.size()) + ",\"rows\":" + datajson + "}";
+        return result;
     }
 }
